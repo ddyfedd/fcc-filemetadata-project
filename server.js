1082fd523bb40fa18f9f756808b7fff143a20ea9
Dávid Zsámboki <andrazsenator@gmail.com>
2021-01-25 16:26:01 +0000

    setting up .env file and basic dependencies, mongodb connection

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var cors = require('cors');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
 require('dotenv').config()
 
 var app = express();
@@ -17,4 +20,19 @@ app.get('/', function (req, res) {
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
+});
+
+
+// connect  to mongoDB
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000
+});
+
+const connection = mongoose.connection;
+connection.on('error', console.error.bind(console, 'connection error: '));
+connection.once('open', () => {
+  console.log("MongoDB connection successful.");
 });
